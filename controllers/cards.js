206bc15cb0d66fb54ad2a12dbbe41bef6f3335ad
Maks Nikulnikov <5286433@gmail.com>
2023-04-18 17:37:23 +0300

    added celebrate

--- controllers/cards.js
+++ controllers/cards.js
@@ -15,7 +15,8 @@ module.exports.getCards = (req, res, next) => {
 module.exports.deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
     .then((searchedCard) => {
-      if (!req.user._id === searchedCard.owner) {
+      console.log(String(req.user._id), searchedCard._id.toString(), !(String(req.user._id) === searchedCard._id.toString()))
+      if (!(String(req.user._id) === searchedCard._id.toString())) {
         return Promise.reject(new ForbiddenError('Вы не можете удалить эту карту'));
       }
       return Card.findByIdAndRemove(req.params.cardId)
