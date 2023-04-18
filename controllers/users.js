2fef56b8c54113803b44e9128577a24cf1bb6b35
Maks Nikulnikov <5286433@gmail.com>
2023-04-18 11:09:45 +0300

    added celebrate

--- controllers/users.js
+++ controllers/users.js
@@ -38,7 +38,15 @@ module.exports.createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send({
+      data: {
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+        _id: user._id,
+      },
+    }))
     .catch((err) => {
       if (err.code === 11000) {
         next(new ConflictError('Пользователь с таким email уже существует'));
