5714eee9f42e9e3d0ba8619f2872052b8d8d10c8
Maks Nikulnikov <5286433@gmail.com>
2023-04-29 16:39:07 +0300

    Added logging

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -10,9 +11,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(requestLogger);
 app.use('/', require('./routes/index'));
 
+app.use(errorLogger);
 app.use(errors());
 app.use(require('./middlewares/errors'));
 
